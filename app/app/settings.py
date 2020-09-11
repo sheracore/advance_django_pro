f8c70bf72665782dd7ed6fa5bbb7ce81744ac038
lion7472 <m.ghaffari662@gmail.com>
2020-09-11 11:54:35 +0430

    Added image field to recipe model

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,6 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# 127.0.0.1:8000/static/
+# 127.0.0.1:8000/media/
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+
+# It means were to save media files
+MEDIA_ROOT = '/vol/web/media/'
+STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
